c0543ed888601b4e2338d3b29e9aef6b2415729d
processor4d <processor4d@126.com>
2019-09-30 17:47:44 +0800

    add data and train.sh

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget http://snap.stanford.edu/jodie/reddit.csv -P data/
-wget http://snap.stanford.edu/jodie/wikipedia.csv -P data/
-wget http://snap.stanford.edu/jodie/mooc.csv -P data/
-wget http://snap.stanford.edu/jodie/lastfm.csv -P data/
+#wget http://snap.stanford.edu/jodie/reddit.csv -P data/
+#wget http://snap.stanford.edu/jodie/wikipedia.csv -P data/
+#wget http://snap.stanford.edu/jodie/mooc.csv -P data/
+curl -o data/mooc.csv http://snap.stanford.edu/jodie/mooc.csv
+#wget http://snap.stanford.edu/jodie/lastfm.csv -P data/
---
+++ train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NETWORK=mooc.csv
+EPOCH=50
+DATE=$( date +%Y_%m_%d_%H_%M_%S )
+MODEL="jodie_${NETOWRK}_${EPOCH}_${DATE}
+python python jodie.py --network ${NETWORK} --model ${MODEL} --epochs ${EPOCH}
