66b016495f7e0b81306ffdaf920f367befc007f5
processor4d <processor4d@126.com>
2019-09-30 18:32:34 +0800

    modify train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NETWORK=mooc.csv
+NETWORK=mooc
 EPOCH=50
 DATE=$( date +%Y_%m_%d_%H_%M_%S )
-MODEL="jodie_${NETOWRK}_${EPOCH}_${DATE}
-python python jodie.py --network ${NETWORK} --model ${MODEL} --epochs ${EPOCH}
+MODEL="jodie_${NETOWRK}_${EPOCH}_${DATE}"
+python jodie.py --network ${NETWORK} --model ${MODEL} --epochs ${EPOCH}
